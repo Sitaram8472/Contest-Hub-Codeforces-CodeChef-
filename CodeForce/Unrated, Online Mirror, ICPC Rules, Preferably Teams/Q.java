9bd7051477076c1499c85e75443f1efac4df3f15
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-11-15 23:34:18 +0530

    update

---
+++ CodeForce/Unrated, Online Mirror, ICPC Rules, Preferably Teams/Q.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Q {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int t = sc.nextInt();
+      while (t-- > 0) {
+       int n = sc.nextInt();
+       int[] num = new int[n];
+       for(int i=0; i<n; i++){
+        num[i] = sc.nextInt();
+        }
+        
+      }
+      sc.close();
+    }
+}
